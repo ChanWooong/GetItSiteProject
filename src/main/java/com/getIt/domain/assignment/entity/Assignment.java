525bfb29fbefa76d4368dd40c47ebfc995e3188d
arcuar <nunnaerim@gmail.com>
2026-02-26 03:31:20 +0900

    feat: Assignment 엔티티 생성 및 Status Enum 추가

---
+++ src/main/java/com/getIt/domain/assignment/entity/Assignment.java
@@ -0,0 +1,70 @@
+package com.getit.domain.assignment.entity;
+
+import com.getit.domain.assignment.Status;
+import com.getit.domain.member.entity.Member;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assignment", uniqueConstraints = {
+    @UniqueConstraint(
+        name = "uk_task_member",
+        columnNames = {"task_id", "member_id"}
+    )
+})
+@Getter
+@DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id", updatable = false, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false, updatable = false)
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false,  updatable = false)
+    private Member member;
+
+    @NotBlank
+    @Column(name = "file_path", nullable = false, length = 255)
+    private String filePath;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Status status = Status.SUBMITTED;
+
+    @CreatedDate
+    @Column(name = "submitted_at", updatable = false, nullable = false)
+    private LocalDateTime submittedAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @Builder
+    private Assignment(Task task, Member member, String filePath) {
+        this.task = task;
+        this.member = member;
+        this.filePath = filePath;
+    }
+}
