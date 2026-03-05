b3dadae675817173667a67072376ff4e361095fb
Y-Jimin <jak2899@naver.com>
2026-03-05 14:54:23 +0900

    feat:임시저장 repository 구조 작성

--- src/main/java/com/getIt/domain/apply/repository/ApplicationRepository.java
+++ src/main/java/com/getIt/domain/apply/repository/ApplicationRepository.java
@@ -5,8 +5,11 @@ import com.getit.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
     List<Application> findAllByMemberOrderByIdDesc(Member member);
+
+    Optional<Application> findFirstByMemberAndIsDraftTrue(Member member);
 }
