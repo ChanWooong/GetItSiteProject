a016309d6c80db38f24d86941bdc496e15b16e7d
Y-Jimin <jak2899@naver.com>
2026-03-05 16:56:30 +0900

    feat:임시저장 불러오기 dto 작성

---
+++ src/main/java/com/getIt/domain/apply/dto/ApplyDraftLoadResponse.java
@@ -0,0 +1,26 @@
+package com.getit.domain.apply.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ApplyDraftLoadResponse {
+
+    private boolean success;
+    private String message;
+    private ApplyDraftDataDto data;
+
+    public static ApplyDraftLoadResponse of(String message, ApplyDraftDataDto data) {
+        return ApplyDraftLoadResponse.builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
