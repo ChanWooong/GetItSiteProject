d4a8bbd79536128ebe1ad891e8a5005dfc73beb9
arcuar <nunnaerim@gmail.com>
2026-03-13 19:35:56 +0900

    feat: Lecture, LectureFile 엔티티 생성

---
+++ src/main/java/com/getIt/domain/lecture/entity/LectureFile.java
@@ -0,0 +1,37 @@
+package com.getit.domain.lecture.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "lecture_file")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LectureFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lecture_file_id", updatable = false, nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id", nullable = false, updatable = false)
+    private Lecture lecture;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String fileName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String filePath;
+
+    @Builder
+    private LectureFile(Lecture lecture, String fileName, String filePath) {
+        this.lecture = lecture;
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+}
