c04c668dd0726520906647c2e3d9ef775425c975
chanwoong <oco466kr@knu.ac.kr>
2026-03-22 23:31:15 +0900

    refactor : Lecture 기반 DTO 파일 위치 이동

--- src/main/java/com/getIt/domain/lecture/dto/LectureCreateRequestDto.java
+++ src/main/java/com/getIt/domain/lecture/dto/LectureCreateRequestDto.java
@@ -1,8 +1,9 @@
-package com.getit.domain.admin.lecture.dto;
+package com.getit.domain.lecture.dto;
 
 import com.getit.domain.assignment.TrackType;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.URL;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,17 +14,19 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class LectureUpdateRequestDto {
+public class LectureCreateRequestDto {
 
-    @Pattern(regexp = ".*\\S.*", message = "제목은 공백만 입력할 수 없습니다.")
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
 
-    @Pattern(regexp = ".*\\S.*", message = "설명은 공백만 입력할 수 없습니다.")
+    @NotBlank(message = "설명은 필수입니다.")
     private String description;
 
+    @NotNull(message = "주차는 필수입니다.")
     @Min(value = 1, message = "주차는 1 이상이어야 합니다.")
     private Integer week;
 
+    @NotNull(message = "트랙 타입은 필수입니다.")
     private TrackType type;
 
     @URL(message = "올바른 URL 형식이어야 합니다.")
